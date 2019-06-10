a6a3085a013b00200b77aa4aa82bee7f9ab47c3a
Lorenz Hänggi <lolo8304@gmail.com>
2019-06-10 03:14:35 +0200

    AR demo - initial place item

---
+++ BloxxsAR-2019-unity/Assets/_Basic/ARTapToPlaceObject.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+//using UnityEngine.Experimental.XR;
+using System;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+
+    public GameObject objectToPlace;
+    public GameObject placementIndicator;
+
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager arRaycastManager;
+    private ARPlaneManager arPlaneManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+        arPlaneManager = FindObjectOfType<ARPlaneManager>();
+        objectToPlace.tag = "Selectable";
+        objectToPlace.transform.tag = "Selectable";
+
+    }
+
+
+    private bool hasValidTouch() {
+        return this.placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+        if (hasValidTouch()) {
+            PlaceObject();
+        }
+    }
+
+    private void PlaceObject()
+    {
+        var placedObject = Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+        placedObject.tag = "Selectable";
+        
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid) {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        } else {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose() {
+
+        //search where rays hit the detected plane with ARKit
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        //var screenRay = Camera.current.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+
+        var hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, hits, trackableTypes: TrackableType.PlaneWithinBounds | TrackableType.PlaneWithinPolygon);
+
+        //var hits = new List<XRRaycastHit>(arPlaneManager.Raycast(screenRay, TrackableType.All, Allocator.None));
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid) {
+            placementPose = hits[0].pose;
+            for (int i = 0; i < hits.Count; i++)
+            {
+                var hit = hits[i];
+                ARPlane hitPlane = arPlaneManager.GetPlane(hit.trackableId);
+                //Console.Write("hit "+i+": type="+hit.hitType+", distance="+hit.distance+", id="+hit.trackableId.ToString()+", found="+(hitPlane != null)); Console.WriteLine();
+                //if (hitPlane) {
+                //    hitPlane.gameObject.SetActive(i == 0);
+                //}
+            }
+            // adapt rotation of placement Indicator not turning to start center but to current angle of front camera
+            var cameraForward = Camera.current.transform.forward;
+            //vectors representing a direction always use "normalized"
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+}
---
+++ BloxxsAR-2019-unity/Assets/_Basic/SelectionManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] public string selectedTag = "Selectable";
+    [SerializeField] public Material highlightMaterial;
+
+    private Transform _selection;
+    private Transform _oldSelection;
+    private Material _selectionMaterial;
+    private Material _oldSelectionMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _selection = null;
+        _oldSelection = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_oldSelection != _selection && _oldSelection != null) {
+            var oldSelectionRenderer = _oldSelection.GetComponentInChildren<Renderer>();
+            if (oldSelectionRenderer != null && _oldSelectionMaterial != null) {
+                oldSelectionRenderer.material = _oldSelectionMaterial;
+                    Console.Write("material reset ("+_oldSelectionMaterial.ToString());Console.WriteLine();
+            }
+            _oldSelectionMaterial = null;
+            _oldSelection = null;
+        }
+        var screenPoint = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var screenRay = Camera.current.ScreenPointToRay(screenPoint);
+        RaycastHit hit;
+
+        _oldSelection = _selection;
+        _oldSelectionMaterial = _selectionMaterial;
+        if (Physics.Raycast(screenRay, out hit)) {
+            var selection = hit.transform;
+            var rootSelection = selection.root;
+            if (rootSelection.CompareTag(selectedTag)) {
+                if (rootSelection != _oldSelection) {
+                    Console.Write("ray hit object tagged="+rootSelection.tag+", name="+rootSelection.name);Console.WriteLine();
+                    var selectionRenderer = rootSelection.GetComponentInChildren<Renderer>();
+                    if (selectionRenderer != null) {
+                        _selectionMaterial = selectionRenderer.material;
+                        selectionRenderer.material = highlightMaterial;
+                        Console.Write("material set");Console.WriteLine();
+                        _selection = rootSelection;
+                    } else {
+                        _selectionMaterial = null;
+                        _selection = null;
+                    }
+                }
+            } else {
+                _selectionMaterial = null;
+                _selection = null;
+            }
+        } else {
+            _selectionMaterial = null;
+            _selection = null;
+        }
+    }
+}
